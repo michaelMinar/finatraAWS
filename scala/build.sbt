c9c4f7bfe141d0cfb684e1f165036972d43e0d8c
michaelMinar <mminar@trifacta.com>
2015-07-21 21:24:56 -0700

    build file and basic.scala

---
+++ scala/build.sbt
@@ -0,0 +1,10 @@
+organization := "com.falstaff"
+
+scalaVersion := "2.10.4"
+
+resolvers ++= Seq(
+    "twttr" at "http://maven.twttr.com/")
+
+libraryDependencies ++= Seq(
+    "com.twitter" %% "finagle-http" % "6.24.0",
+)
---
+++ scala/src/main/scala/com/falstaff/basic.scala
@@ -0,0 +1,23 @@
+package com.falstaff
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.util.{Await, Future}
+
+/** a remarkably simple service for returning success */
+class simpleService extends Service[HttpRequest, HttpResponse] {
+    def apply(req: HttpRequest): Future[HttpResponse] =
+      Future.value(new DefaultHttpResponse(
+        req.getProtocolVersion, HttpResponseStatus.OK))
+      .onSuccess{ y=> println("success") }
+}
+
+/** 'simple' http server, finagle-style */
+object Server extends App {
+
+  // create an instance of the server
+  val service = new simpleService
+
+  // run the server
+  val server = Http.serve(":8080", service)
+  Await.ready(server)
+}
